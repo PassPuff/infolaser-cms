93e3a135aba4ab451a51b90b3f49678729ead6a5
Nikita <passpuffnikita@gmail.com>
2025-03-03 11:40:01 +0300

    Configure Vite server to listen on port 3000 and allow external connections

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		port: 3000, // Задаем нужный порт (например, 3000)
+		host: true // Позволяет слушать внешние подключения
+	}
 });
