947a77737b9243b8612f736ac3ef18efe9c721ea
Nikita <passpuffnikita@gmail.com>
2025-03-03 11:42:30 +0300

    Update Vite server configuration to use port 4173

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 3000, // Задаем нужный порт (например, 3000)
-		host: true // Позволяет слушать внешние подключения
+		port: 4173,
+		host: true
 	}
 });
