856e6d0d132031c8bea53f4e3fc0221cca37bdff
Nikita <passpuffnikita@gmail.com>
2025-03-12 16:22:36 +0300

    Добавляет алиас для пути и улучшает стилизацию компонентов.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { useLogout } from "@refinedev/core";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 
 export const Header = () => {
-	const { mutate, isLoading } = useLogout();
+  const { mutate, isLoading } = useLogout();
 
-	return (
-		<header className="flex items-center justify-between max-w-3xl">
-			<h2>Welcome!</h2>
-			<Button
-				type="button"
-				variant="destructive"
-				disabled={isLoading}
-				onClick={mutate}
-			>
-				Logout
-			</Button>
-		</header>
-	);
+  return (
+    <header className="flex items-center justify-between max-w-3xl">
+      <h2>Welcome!</h2>
+      <Button
+        type="button"
+        variant="destructive"
+        disabled={isLoading}
+        onClick={() => mutate()}
+      >
+        Logout
+      </Button>
+    </header>
+  );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,46 +1,48 @@
 import React from "react";
 import { useLogin } from "@refinedev/core";
 import { Button } from "../components/ui/button";
-
+import { Input } from "@/components/ui/input";
 
 export const Login = () => {
-	const { mutate, isLoading } = useLogin();
+  const { mutate, isLoading } = useLogin();
 
-	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		// Using FormData to get the form values and convert it to an object.
-		const data = Object.fromEntries(new FormData(event.target).entries());
-		// Calling mutate to submit with the data we've collected from the form.
-		mutate(data);
-	};
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Using FormData to get the form values and convert it to an object.
+    const data = Object.fromEntries(
+      new FormData(event.currentTarget).entries(),
+    );
+    // Calling mutate to submit with the data we've collected from the form.
+    mutate(data);
+  };
 
-	return (
-		<div>
-			<h1>Login</h1>
-			<form onSubmit={onSubmit}>
-				<label htmlFor="email">Email</label>
-				<input
-					type="email"
-					id="email"
-					name="email"
-					// We're providing default values for demo purposes.
-					defaultValue="admin@admin.com"
-				/>
+  return (
+    <div>
+      <h1 className="text-4xl py-10 text-center">Login</h1>
+      <form className="max-w-xl m-auto" onSubmit={onSubmit}>
+        <label htmlFor="email">Email</label>
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          // We're providing default values for demo purposes.
+          defaultValue="admin@admin.com"
+        />
 
-				<label htmlFor="password">Password</label>
-				<input
-					type="password"
-					id="password"
-					name="password"
-					// We're providing default values for demo purposes.
-					defaultValue="123456"
-				/>
+        <label htmlFor="password">Password</label>
+        <Input
+          type="password"
+          id="password"
+          name="password"
+          // We're providing default values for demo purposes.
+          defaultValue="123456"
+        />
 
-				{isLoading && <span>loading...</span>}
-				<Button type="submit" disabled={isLoading}>
-					Submit
-				</Button>
-			</form>
-		</div>
-	);
+        {isLoading && <span>loading...</span>}
+        <Button className="mt-5" type="submit" disabled={isLoading}>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 };
